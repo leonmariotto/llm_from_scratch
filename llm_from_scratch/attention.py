57d5f887959ca4dc65a2ea55980a37f960b13329
Leon Mariotto <leon.mariotto@provenrun.com>
2026-04-11 16:24:58 +0700

    Improve attention module.
    Add CausalAttention and MultiHead, add corresponding tests.

--- llm_from_scratch/attention.py
+++ llm_from_scratch/attention.py
@@ -1,10 +1,13 @@
 """
-Implement a simple self attention mechanism.
-Use scaled-dot product attention.
-With activable features :
-    - Causal attention mask (TODO)
-    - Dropout (TODO)
-    - Multi-head attention (TODO)
+Implement a simple self attention.
+
+3 version :
+    - Attention: bare-minimal, roughly usable.
+    - CausalAttention: implement causal attention mask and dropout.
+    - MultiHeadAttention: implement causal attention mask and dropout and
+        optimize with N batched matrix multiplication (head) parralelizable.
+
+Attention mechanism involve 3 trainable matrix : query, key, values.
 """
 
 import torch
@@ -48,4 +51,177 @@ class Attention(nn.Module):
 
         # Finaly, compute the context vector.
         context_vec = attn_weights @ values
+        return context_vec
+
+
+class CausalAttention(nn.Module):
+    """
+    Implement Causal mask and dropout.
+    Parameters :
+        - d_in: embedding size (size of embedded vector, 1 embedded vector per token)
+        - d_out context vector size.
+        - context_lenght: correspond to the number token used to compute a context vector.
+        In the case of a DataSet/DataLoader setup, it will correspond to the window_size.
+        - droput: for training purpose, it is possible to hide randomly some attention weight before
+        computing the context vector. dropout value is the probability for a weight to be zeroed.
+
+    """
+
+    # Need to tell pyright that the "mask" registered by register_buffer method
+    # is an tensor, to avoid typing errors.
+    mask: torch.Tensor
+
+    def __init__(
+        self,
+        d_in: int,
+        d_out: int,
+        context_length: int = 1,
+        dropout: float = 0.0,
+        qkv_bias: bool = False,
+    ):
+        super().__init__()
+        # bias is the (trainable) b parameter in y = Wx + b
+        # without it the model is less flexible.
+        # qkv = query, key, value
+        self.W_query = nn.Linear(d_in, d_out, bias=qkv_bias)
+        self.W_key = nn.Linear(d_in, d_out, bias=qkv_bias)
+        self.W_value = nn.Linear(d_in, d_out, bias=qkv_bias)
+        self.dropout = nn.Dropout(p=dropout)
+        self.register_buffer(
+            "mask", torch.triu(torch.ones(context_length, context_length), diagonal=1)
+        )
+
+    def forward(self, x):
+        """
+        forward method is called by the nn.Module __call__ method.
+        x is expected to be a batch of tensor of d_in size.
+        (number of batch, number of token per sample (context_length), embedding size)
+        return a tensor of d_out size.
+        """
+        _, num_tokens, _ = x.shape  # New batch dimension b
+        keys = self.W_key(x)
+        queries = self.W_query(x)
+        values = self.W_value(x)
+
+        attn_scores = queries @ keys.transpose(1, 2)
+        # Softmax function converts its input into a probability distribution, -inf is found
+        # it treat it as 0.
+        attn_scores.masked_fill_(
+            # `:num_tokens` to account for cases where the number of tokens in the batch is smaller
+            # than the supported context_size
+            self.mask[:num_tokens, :num_tokens].to(torch.bool),
+            -torch.inf,
+        )
+        # Then, normalize the attention score with softmax function
+        # Scale by the square root of the embedding dimension.
+        attn_weights = torch.softmax(attn_scores / keys.shape[-1] ** 0.5, dim=-1)
+        attn_weights = self.dropout(attn_weights)
+
+        # Finaly, compute the context vector.
+        context_vec = attn_weights @ values
+        return context_vec
+
+
+class MultiHeadAttention(nn.Module):
+    """
+    The key thing here is that d_out is splited in num_head parts. Each head produce a part
+    of d_out (head_dim, calculated at init), and at the end context_vec is reshaped to the correct size.
+    So things can be parallel.
+    """
+
+    # Need to tell pyright that the "mask" registered by register_buffer method
+    # is an tensor, to avoid typing errors.
+    mask: torch.Tensor
+
+    def __init__(
+        self,
+        d_in: int,
+        d_out: int,
+        context_length: int,
+        dropout: float,
+        num_heads: int,
+        qkv_bias=False,
+    ):
+        super().__init__()
+
+        assert num_heads != 0, "num_head shall not be 0"
+        assert d_out % num_heads == 0, "d_out must be divisible by num_heads"
+
+        self.d_out = d_out
+        self.d_in = d_in
+        self.num_heads = num_heads
+        self.head_dim = (
+            d_out // num_heads
+        )  # Reduce the projection dim to match desired output dim
+        self.context_length = context_length
+
+        self.W_query = nn.Linear(d_in, d_out, bias=qkv_bias)
+        self.W_key = nn.Linear(d_in, d_out, bias=qkv_bias)
+        self.W_value = nn.Linear(d_in, d_out, bias=qkv_bias)
+        self.out_proj = nn.Linear(d_out, d_out)  # Linear layer to combine head outputs
+        self.dropout = nn.Dropout(dropout)
+        self.register_buffer(
+            "mask", torch.triu(torch.ones(context_length, context_length), diagonal=1)
+        )
+
+    def forward(self, x):
+        b, num_tokens, d_in = x.shape
+
+        assert self.context_length == num_tokens, "invalid d_in (embedding size)"
+        assert self.d_in == d_in, "invalid d_in (embedding size)"
+
+        # As in `CausalAttention`, for inputs where `num_tokens` exceeds `context_length`,
+        # this will result in errors in the mask creation further below.
+        # In practice, this is not a problem since the LLM (chapters 4-7) ensures that inputs
+        # do not exceed `context_length` before reaching this forward method.
+
+        keys = self.W_key(x)  # Shape: (b, num_tokens, d_out)
+        queries = self.W_query(x)
+        values = self.W_value(x)
+
+        # NOTE on tensor.view and tensor.transpose methods.
+        # Tensor view method reshape a tensor, without moving elements in memory.
+        # Whereas transpose change how dimensions are indexed.
+        # So, for example :
+        #     tensor([[0, 1, 2],
+        #         [3, 4, 5]])
+        # y.view(3,2)
+        #     tensor([[0, 1],
+        #         [2, 3],
+        #         [4, 5]])
+        # y.transpose(0, 1)
+        #     tensor([[0, 3],
+        #             [1, 4],
+        #             [2, 5]])
+
+        # We implicitly split the matrix by adding a `num_heads` dimension
+        # Unroll last dim: (b, num_tokens, d_out) -> (b, num_tokens, num_heads, head_dim)
+        keys = keys.view(b, num_tokens, self.num_heads, self.head_dim)
+        values = values.view(b, num_tokens, self.num_heads, self.head_dim)
+        queries = queries.view(b, num_tokens, self.num_heads, self.head_dim)
+
+        # Transpose: (b, num_tokens, num_heads, head_dim) -> (b, num_heads, num_tokens, head_dim)
+        keys = keys.transpose(1, 2)
+        queries = queries.transpose(1, 2)
+        values = values.transpose(1, 2)
+
+        # Compute scaled dot-product attention (aka self-attention) with a causal mask
+        attn_scores = queries @ keys.transpose(2, 3)  # Dot product for each head
+
+        # Original mask truncated to the number of tokens and converted to boolean
+        mask_bool = self.mask.bool()[:num_tokens, :num_tokens]
+
+        # Use the mask to fill attention scores
+        attn_scores.masked_fill_(mask_bool, -torch.inf)
+
+        attn_weights = torch.softmax(attn_scores / keys.shape[-1] ** 0.5, dim=-1)
+        attn_weights = self.dropout(attn_weights)
+
+        # Shape: (b, num_tokens, num_heads, head_dim)
+        context_vec = (attn_weights @ values).transpose(1, 2)
+
+        # Combine heads, where self.d_out = self.num_heads * self.head_dim
+        context_vec = context_vec.contiguous().view(b, num_tokens, self.d_out)
+        context_vec = self.out_proj(context_vec)  # optional projection
+
         return context_vec
--- tests/test_attention.py
+++ tests/test_attention.py
@@ -1,5 +1,11 @@
+"""
+Tests attention.py classes.
+Test use torch.manual_seed so context vector output are reproducible.
+To add a test just print the output using outputs.tolist() and then use it
+as "expected_output".
+"""
 import torch
-from ..llm_from_scratch.attention import Attention
+from ..llm_from_scratch.attention import Attention, CausalAttention, MultiHeadAttention
 
 
 def test_attention() -> None:
@@ -34,3 +40,142 @@ def test_attention() -> None:
     assert outputs.shape == (inputs.shape[0], d_out)
     # With a fixed seed, the layer initialization is deterministic, so the output is too.
     torch.testing.assert_close(outputs, expected_outputs)
+
+def test_attention_with_causal_mask() -> None:
+    torch.manual_seed(42)  # Let there be order among chaos.
+    d_in = 3
+    d_out = 2
+    inputs = torch.tensor(
+        [
+            [0.43, 0.15, 0.89],  # Your     (x^1)
+            [0.55, 0.87, 0.66],  # journey  (x^2)
+            [0.57, 0.85, 0.64],  # starts   (x^3)
+            [0.22, 0.58, 0.33],  # with     (x^4)
+            [0.77, 0.25, 0.10],  # one      (x^5)
+            [0.05, 0.80, 0.55],  # step     (x^6)
+        ],
+        requires_grad=False,
+    )
+    # 2 inputs with 6 tokens each, and each token has embedding dimension 3
+    batch = torch.stack((inputs, inputs), dim=0)
+    context_length = batch.shape[1]
+    sa_v2 = CausalAttention(d_in, d_out, context_length, 0.0)
+    context_vecs = sa_v2(batch)
+    expected_context_vecs = torch.tensor(
+        [
+            [
+                [0.44291412830352783, 0.10765889286994934],
+                [0.4656165540218353, 0.25971394777297974],
+                [0.47317469120025635, 0.30297863483428955],
+                [0.4135492742061615, 0.29212889075279236],
+                [0.4078015983104706, 0.25666138529777527],
+                [0.3771984279155731, 0.274605393409729],
+            ],
+            [
+                [0.44291412830352783, 0.10765889286994934],
+                [0.4656165540218353, 0.25971394777297974],
+                [0.47317469120025635, 0.30297863483428955],
+                [0.4135492742061615, 0.29212889075279236],
+                [0.4078015983104706, 0.25666138529777527],
+                [0.3771984279155731, 0.274605393409729],
+            ],
+        ]
+    )
+    assert context_vecs.shape == (batch.shape[0], batch.shape[1], d_out)
+    torch.testing.assert_close(context_vecs, expected_context_vecs)
+
+def test_attention_with_causal_mask_and_dropout() -> None:
+    torch.manual_seed(42)  # Let there be order among chaos.
+    d_in = 3
+    d_out = 2
+    inputs = torch.tensor(
+        [
+            [0.43, 0.15, 0.89],  # Your     (x^1)
+            [0.55, 0.87, 0.66],  # journey  (x^2)
+            [0.57, 0.85, 0.64],  # starts   (x^3)
+            [0.22, 0.58, 0.33],  # with     (x^4)
+            [0.77, 0.25, 0.10],  # one      (x^5)
+            [0.05, 0.80, 0.55],  # step     (x^6)
+        ],
+        requires_grad=False,
+    )
+    # 2 inputs with 6 tokens each, and each token has embedding dimension 3
+    batch = torch.stack((inputs, inputs), dim=0)
+    context_length = batch.shape[1]
+    sa_v2 = CausalAttention(d_in, d_out, context_length, dropout=0.3)
+    context_vecs = sa_v2(batch)
+    expected_context_vecs = torch.tensor(
+        [
+            [
+                [0.632734477519989, 0.1537984162569046],
+                [0.6651664972305298, 0.3710199296474457],
+                [0.6759638786315918, 0.4328266382217407],
+                [0.5907846689224243, 0.41732701659202576],
+                [0.5148074626922607, 0.2879510819911957],
+                [0.3608359098434448, 0.19555935263633728]],
+            [
+                [0.0, 0.0],
+                [0.6651664972305298, 0.3710199296474457],
+                [0.6759638786315918, 0.4328266382217407],
+                [0.5907846689224243, 0.41732701659202576],
+                [0.43168559670448303, 0.24710917472839355],
+                [0.5388548970222473, 0.39229339361190796]]
+        ]
+    )
+    assert context_vecs.shape == (batch.shape[0], batch.shape[1], d_out)
+    torch.testing.assert_close(context_vecs, expected_context_vecs)
+
+
+def test_multi_head_attention() -> None:
+    torch.manual_seed(42)  # Let there be order among chaos.
+    d_in = 3
+    d_out = 8
+    num_heads = 2
+    inputs = torch.tensor(
+        [
+            [0.43, 0.15, 0.89],  # Your     (x^1)
+            [0.55, 0.87, 0.66],  # journey  (x^2)
+            [0.57, 0.85, 0.64],  # starts   (x^3)
+            [0.22, 0.58, 0.33],  # with     (x^4)
+            [0.77, 0.25, 0.10],  # one      (x^5)
+            [0.05, 0.80, 0.55],  # step     (x^6)
+        ],
+        requires_grad=False,
+    )
+    batch = torch.stack((inputs, inputs), dim=0)
+    context_length = batch.shape[1]
+    mha = MultiHeadAttention(d_in, d_out, context_length, dropout=0.0, num_heads=num_heads)
+    context_vecs = mha(batch)
+    expected_context_vecs = torch.tensor(
+        [
+            [
+                [-0.5691065192222595, -0.26063844561576843, 0.2583349347114563, -0.23653504252433777, 0.00406038761138916, -0.3905044496059418, 0.2901259660720825, -0.43187201023101807],
+                [-0.5130563974380493, -0.31158339977264404, 0.19848865270614624, -0.2949388027191162, 0.022025778889656067, -0.41800007224082947, 0.26127344369888306, -0.33525028824806213],
+                [-0.49515604972839355, -0.3222957253456116, 0.17558138072490692, -0.3131128251552582, 0.030412942171096802, -0.42242148518562317, 0.2522827982902527, -0.3009388744831085],
+                [-0.4675266444683075, -0.3366760015487671, 0.16150547564029694, -0.3028821349143982, 0.03932690620422363, -0.41380324959754944, 0.24549350142478943, -0.28214797377586365],
+                [-0.43044808506965637, -0.3088979125022888, 0.12365879118442535, -0.2530515193939209, 0.08146975934505463, -0.37486952543258667, 0.257982462644577, -0.2569080591201782],
+                [-0.4332759976387024, -0.33743706345558167, 0.1384759545326233, -0.2743425965309143, 0.06177394092082977, -0.3959888219833374, 0.24754902720451355, -0.2602173089981079],
+            ],
+            [
+                [-0.5691065192222595, -0.26063844561576843, 0.2583349347114563, -0.23653504252433777, 0.00406038761138916, -0.3905044496059418, 0.2901259660720825, -0.43187201023101807],
+                [-0.5130563974380493, -0.31158339977264404, 0.19848865270614624, -0.2949388027191162, 0.022025778889656067, -0.41800007224082947, 0.26127344369888306, -0.33525028824806213],
+                [-0.49515604972839355, -0.3222957253456116, 0.17558138072490692, -0.3131128251552582, 0.030412942171096802, -0.42242148518562317, 0.2522827982902527, -0.3009388744831085],
+                [-0.4675266444683075, -0.3366760015487671, 0.16150547564029694, -0.3028821349143982, 0.03932690620422363, -0.41380324959754944, 0.24549350142478943, -0.28214797377586365],
+                [-0.43044808506965637, -0.3088979125022888, 0.12365879118442535, -0.2530515193939209, 0.08146975934505463, -0.37486952543258667, 0.257982462644577, -0.2569080591201782],
+                [-0.4332759976387024, -0.33743706345558167, 0.1384759545326233, -0.2743425965309143, 0.06177394092082977, -0.3959888219833374, 0.24754902720451355, -0.2602173089981079],
+            ],
+        ]
+    )
+    assert context_vecs.shape == (batch.shape[0], batch.shape[1], d_out)
+    torch.testing.assert_close(context_vecs, expected_context_vecs)
+
+def test_multi_head_attention_gpt2_scaled() -> None:
+    d_in = 768
+    d_out = 768
+    num_heads = 12
+    context_length = 1024
+    batch_count = 5
+    batch = torch.rand(batch_count, context_length, d_in)
+    mha = MultiHeadAttention(d_in, d_out, context_length, dropout=0.0, num_heads=num_heads)
+    context_vecs = mha(batch)
+    assert context_vecs.shape == (batch.shape[0], batch.shape[1], d_out)
